3daf94649187029ac13e06b283a7774abed4d886
Marcel Stadelmann <m@marceli.to>
2022-04-04 13:25:10 +0200

    bugfix backers

--- resources/views/pages/member/partials/backers.blade.php
+++ resources/views/pages/member/partials/backers.blade.php
@@ -3,13 +3,13 @@
     <h1>Gönner*Innen</h1>
     @if ($backers)
       <div class="columns">
-        @if ($backers['Personen'])
+        @if (isset($backers['Personen']))
           <h2 class="lg:mb-0">Personen</h2>
           @foreach($backers['Personen'] as $backer)
             <div class="mb-1x">{{ $backer->full }}</div>
           @endforeach
         @endif
-        @if ($backers['Firmen'])
+        @if (isset($backers['Firmen']))
           <h2 class="mt-8x md:mt-10x lg:mb-0">Firmen</h2>
           @foreach($backers['Firmen'] as $backer)
             <div class="mb-1x">{{ $backer->full }}</div>
