c1c7801dc4547a9b19a3ecf6e88ed2d0cf6c7044
Marcel Stadelmann <m@marceli.to>
2022-04-05 15:20:54 +0200

    bugfix backers & network

--- resources/views/pages/member/partials/backers.blade.php
+++ resources/views/pages/member/partials/backers.blade.php
@@ -1,21 +1,21 @@
+@if ($backers)
 <section class="content">
   <div>
     <h1>Gönner*Innen</h1>
-    @if ($backers)
-      <div class="columns">
-        @if (isset($backers['Personen']))
-          <h2 class="lg:mb-0">Personen</h2>
-          @foreach($backers['Personen'] as $backer)
-            <div class="mb-1x">{{ $backer->full }}</div>
-          @endforeach
-        @endif
-        @if (isset($backers['Firmen']))
-          <h2 class="mt-8x md:mt-10x lg:mb-0">Firmen</h2>
-          @foreach($backers['Firmen'] as $backer)
-            <div class="mb-1x">{{ $backer->full }}</div>
-          @endforeach
-        @endif
-      </div>
-    @endif
+    <div class="columns">
+      @if (isset($backers['Personen']))
+        <h2 class="lg:mb-0">Personen</h2>
+        @foreach($backers['Personen'] as $backer)
+          <div class="mb-1x">{{ $backer->full }}</div>
+        @endforeach
+      @endif
+      @if (isset($backers['Firmen']))
+        <h2 class="mt-8x md:mt-10x lg:mb-0">Firmen</h2>
+        @foreach($backers['Firmen'] as $backer)
+          <div class="mb-1x">{{ $backer->full }}</div>
+        @endforeach
+      @endif
+    </div>
   </div>
 </section>
+@endif
--- resources/views/pages/member/partials/network.blade.php
+++ resources/views/pages/member/partials/network.blade.php
@@ -1,16 +1,16 @@
+@if ($partners)
 <section class="content">
   <div>
     <h1>Netzwerk</h1>
-    @if ($partners)
-      @foreach($partners as $partner)
-        <div class="mb-8x md:mb-10x">
-          @if ($partner->url)
-            <a href="{{ $partner->url }}" target="_blank" title="Webseite {{ $partner->name }}">{{ $partner->name }}<br>{{ $partner->url }}</a>
-          @else
-            {{ $partner->name }}
-          @endif
-        </div>
-      @endforeach
-    @endif
+    @foreach($partners as $partner)
+      <div class="mb-8x md:mb-10x">
+        @if ($partner->url)
+          <a href="{{ $partner->url }}" target="_blank" title="Webseite {{ $partner->name }}">{{ $partner->name }}<br>{{ $partner->url }}</a>
+        @else
+          {{ $partner->name }}
+        @endif
+      </div>
+    @endforeach
   </div>
 </section>
+@endif
