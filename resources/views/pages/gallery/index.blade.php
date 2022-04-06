eeb8698f5eb04eff5539da60c534fc57d975b1e5
Marcel Stadelmann <m@marceli.to>
2022-04-06 14:42:36 +0200

    staging build

--- resources/views/pages/gallery/index.blade.php
+++ resources/views/pages/gallery/index.blade.php
@@ -2,27 +2,7 @@
 @section('content')
 <section class="content-lightbox">
   <div>
-    <nav class="lightbox">
-      <ul>
-        <li>
-          @if ($browse['prev'])
-            <a href="{{ route('page.gallery', ['page' => $browse['prev']['page']->slug, 'article' => $browse['prev']['article']->id, 'gallery' => $browse['prev']['gallery']->id, 'gallery_slug' => $browse['prev']['gallery']->slug]) }}">
-              <x-icon type="chevron-left-lightbox" />
-            </a>
-          @endif
-        </li>
-        <li>
-          @if ($browse['next'])
-            <a href="{{ route('page.gallery', ['page' => $browse['next']['page']->slug, 'article' => $browse['next']['article']->id, 'gallery' => $browse['next']['gallery']->id, 'gallery_slug' => $browse['next']['gallery']->slug]) }}">
-              <x-icon type="chevron-right-lightbox" />
-            </a>
-          @endif
-        </li>
-      </ul>
-      <a href="/{{ $page->slug }}/#{{$gallery->slug}}" class="btn-lightbox-close">
-        <x-icon type="cross-lightbox" />
-      </a>
-    </nav>
+    @include('pages.gallery.partials.browse', ['class' => 'is-top', 'hasClose' => TRUE])
   </div>
   @if ($gallery)
     <div>
@@ -42,5 +22,8 @@
       @endif
     </div>
   @endif
+  <div>
+    @include('pages.gallery.partials.browse', ['class' => 'is-bottom', 'hasClose' => FALSE])
+  </div>
 </section>
 @endsection
---
+++ resources/views/pages/gallery/partials/browse.blade.php
@@ -0,0 +1,41 @@
+<nav class="lightbox {{ $class ?? $class }}">
+  <ul>
+    <li>
+      @if ($browse['prev'])
+        <a 
+          href="{{ route('page.gallery', 
+            [
+              'page' => $browse['prev']['page']->slug, 
+              'article' => $browse['prev']['article']->id, 
+              'gallery' => $browse['prev']['gallery']->id, 
+              'gallery_slug' => $browse['prev']['gallery']->slug
+            ]
+          )}}"
+          title="{{ $browse['prev']['gallery']->title }}">
+          <x-icon type="chevron-left-lightbox" />
+        </a>
+      @endif
+    </li>
+    <li>
+      @if ($browse['next'])
+        <a 
+          href="{{ route('page.gallery', 
+            [
+              'page' => $browse['next']['page']->slug, 
+              'article' => $browse['next']['article']->id, 
+              'gallery' => $browse['next']['gallery']->id, 
+              'gallery_slug' => $browse['next']['gallery']->slug
+            ]
+          )}}"
+          title="{{ $browse['next']['gallery']->title }}">
+          <x-icon type="chevron-right-lightbox" />
+        </a>
+      @endif
+    </li>
+  </ul>
+  @if ($hasClose)
+    <a href="/{{ $page->slug }}/#{{$gallery->slug}}" class="btn-lightbox-close" title="Zurück">
+      <x-icon type="cross-lightbox" />
+    </a>
+  @endif
+</nav>
