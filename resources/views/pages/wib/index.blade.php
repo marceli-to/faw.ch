78c1002db8b4497d4a6b0b5d162fc1a94134f282
Marcel Stadelmann <m@marceli.to>
2022-05-30 18:31:46 +0200

    update

--- resources/views/pages/wib/index.blade.php
+++ resources/views/pages/wib/index.blade.php
@@ -43,7 +43,7 @@
   <section class="content">
     <div>
       @foreach($page->publishedArticles as $article)
-        <article class="text-media">
+        <article class="text-media" id="{{ \Str::slug($article->title)}}">
           @if ($article->publishedImage)
             <x-image :maxSizes="[0 => 900]" width="900" height="600" :image="$article->publishedImage" ratio="3x2" wrapperClass="lg:span-6" />
           @endif
