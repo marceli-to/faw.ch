47854b518f3681bb81351f7bb53a414b0561d08d
Marcel Stadelmann <m@marceli.to>
2022-06-07 14:06:43 +0200

    fix media gallery

--- resources/views/pages/event/partials/teaser_sws_stadtmodell.blade.php
+++ resources/views/pages/event/partials/teaser_sws_stadtmodell.blade.php
@@ -14,8 +14,8 @@
     <p>Vorhang auf für das atemberaubend wilde Stadtmodell der Künstler Dominik Heim und Ron Temperli! Ganze zehn Monate haben die beiden mit viel Verpackungsmaterial gearbeitet und ein 8x15 Meter grosses, begehbares Stadtmodell wachsen lassen. Mitgewirkt haben auch interessierte Winterthurerinnen und Winterthurer — die sogenannten Stadtwerkstätter.</p>
     <p>Dominik Heim, geboren 1974, lebt und arbeitet als freischaffender Künstler mit Schwerpunkt Malerei in Winterthur. Ron Temperli, geboren 1975, lebt und arbeitet als freischaffender Künstler mit Schwerpunkt Malerei und Installation in Winterthur.</p>
     <div class="text-media__links">
-      <x-link-gallery page="stadtwerkstatt" :id="17" />
       <x-link-gallery page="stadtwerkstatt" :id="20" />
+      <x-link-gallery page="stadtwerkstatt" :id="21" />
     </div>
   </div>
 </article>
