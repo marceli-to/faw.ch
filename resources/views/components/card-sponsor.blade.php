04a66733b0234ddae4b54d709ece98613b668a4e
Marcel Stadelmann <m@marceli.to>
2022-06-13 13:33:04 +0200

    update member logos

--- resources/views/components/card-sponsor.blade.php
+++ resources/views/components/card-sponsor.blade.php
@@ -1,5 +1,5 @@
 <div class="card-sponsor" >
-  <a href="" target="_blank" title="{{ $title ?? '' }}">
-    <img src="/assets/media/sponsoren/{{ $logo }}" height="200" width="400" {{$attributes }}>
+  <a href="{{ $uri }}" target="_blank" title="{{ $title ?? '' }}">
+    <img src="/assets/media/sponsoren/{{ $logo }}" height="200" width="400" {{ $attributes }}>
   </a>
 </div>
--- resources/views/pages/member/partials/sponsors.blade.php
+++ resources/views/pages/member/partials/sponsors.blade.php
@@ -2,28 +2,28 @@
   <div>
     <h1>Sponsor*Innen</h1>
     <div class="grid grid-cols-12">
-      <x-card-sponsor logo="afebwiw.svg" title="" uri="https://" class="max-w-90" />
-      <x-card-sponsor logo="axa-winterthur.svg" title="" uri="https://" class="max-w-70" />
-      <x-card-sponsor logo="baltensberger.svg" title="" uri="https://" class="max-w-75" />
-      <x-card-sponsor logo="brunner-parkett.svg" title="" uri="https://" class="max-w-90"/>
-      <x-card-sponsor logo="bwt.svg" title="" uri="https://" class="max-w-70" />
-      <x-card-sponsor logo="de-donno.png" title="" uri="https://" class="max-w-75" />
-      <x-card-sponsor logo="elibag.svg" title="" uri="https://" class="max-w-60" />
-      <x-card-sponsor logo="gwg.png" title="" uri="https://" class="max-w-60" />
-      <x-card-sponsor logo="handwerk-am-bau.png" title="" uri="https://" class="max-w-80" />
-      <x-card-sponsor logo="hofmann-gruppe.svg" title="" uri="https://" class="max-w-90" />
-      <x-card-sponsor logo="hug-baustoffe.svg" title="" uri="https://"  class="max-w-65" />
-      <x-card-sponsor logo="hyposound.png" title="" uri="https://" class="max-w-90" />
-      <x-card-sponsor logo="hwg.svg" title="" uri="https://" class="max-w-70" />
-      <x-card-sponsor logo="keller.png" title="" uri="https://" class="max-w-65" />
-      <x-card-sponsor logo="implenia.svg" title="" uri="https://" class="max-w-80" />
-      <x-card-sponsor logo="landolt.svg" title="" uri="https://" class="max-w-90" />
-      <x-card-sponsor logo="russo.svg" title="" uri="https://" class="max-w-85" />
-      <x-card-sponsor logo="staehli.png" title="" uri="https://" class="max-w-90" />
-      <x-card-sponsor logo="swica.svg" title="" uri="https://" class="max-w-70" />
-      <x-card-sponsor logo="thomas-rickli.svg" title="" uri="https://" class="max-w-75" />
-      <x-card-sponsor logo="truninger.png" title="" uri="https://" class="max-w-70" />
-      <x-card-sponsor logo="tuffli-baurecht.png" title="" uri="https://" class="max-w-70" />
+      <x-card-sponsor logo="afebwiw.svg" title="" uri="https://www.gebw.ch" class="max-w-90" />
+      <x-card-sponsor logo="axa-winterthur.svg" title="" uri="https://www.axa.ch/de/privatkunden.html" class="max-w-70" />
+      <x-card-sponsor logo="baltensberger.svg" title="" uri="https://www.baltenspergerbau.ch" class="max-w-75" />
+      <x-card-sponsor logo="brunner-parkett.svg" title="" uri="https://www.brunnersewi.ch" class="max-w-90"/>
+      <x-card-sponsor logo="bwt.svg" title="" uri="https://www.bwt.ch" class="max-w-70" />
+      <x-card-sponsor logo="de-donno.png" title="" uri="http://www.gipser-dedonno.ch" class="max-w-75" />
+      <x-card-sponsor logo="elibag.svg" title="" uri="https://www.elibag.ch" class="max-w-60" />
+      <x-card-sponsor logo="gwg.png" title="" uri="https://gwg.ch" class="max-w-60" />
+      <x-card-sponsor logo="handwerk-am-bau.png" title="" uri="https://www.schweizerag.com/" class="max-w-80" />
+      <x-card-sponsor logo="hofmann-gruppe.svg" title="" uri="https://hofmanngruppe.ch" class="max-w-90" />
+      <x-card-sponsor logo="hug-baustoffe.svg" title="" uri="https://www.hug-baustoffe.ch/"  class="max-w-65" />
+      <x-card-sponsor logo="hyposound.png" title="" uri="https://hyposound.ch" class="max-w-90" />
+      <x-card-sponsor logo="hwg.svg" title="" uri="https://www.hgw-wohnen.ch/" class="max-w-70" />
+      <x-card-sponsor logo="keller.png" title="" uri="https://keller-unternehmungen.ch/de-ch/keller-unternehmungen.html" class="max-w-65" />
+      <x-card-sponsor logo="implenia.svg" title="" uri="https://implenia.com" class="max-w-80" />
+      <x-card-sponsor logo="landolt.svg" title="" uri="https://www.landolt-maler.ch" class="max-w-90" />
+      <x-card-sponsor logo="russo.svg" title="https://gipser-russo.ch" uri="" class="max-w-85" />
+      <x-card-sponsor logo="staehli.png" title="" uri="http://www.staehli-haustechnik.ch/home" class="max-w-90" />
+      <x-card-sponsor logo="swica.svg" title="" uri="https://www.swica.ch" class="max-w-70" />
+      <x-card-sponsor logo="thomas-rickli.svg" title="" uri="https://thomasrickli.ch" class="max-w-75" />
+      <x-card-sponsor logo="truninger.png" title="" uri="https://www.truninger-plot24.ch" class="max-w-70" />
+      <x-card-sponsor logo="tuffli-baurecht.png" title="" uri="https://www.tufflibaurecht.ch" class="max-w-70" />
     </div>
   </div>
 </section>
