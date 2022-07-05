4359104471737e329d92cc2d2ecf5102a75c296c
Marcel Stadelmann <m@marceli.to>
2022-07-05 19:54:09 +0200

    bf

--- resources/views/pages/event/calendar.blade.php
+++ resources/views/pages/event/calendar.blade.php
@@ -102,9 +102,11 @@
   @if ($annual_program->publishedArticlesSpecial)
   <section class="content">
     <div>
-      <article class="teaser">
-        <x-heading type="h2" title="Forum Spezial" subtitle="Weitere Veranstaltungen" class="mb-10x lg:mb-12x" />
-      </article>
+      <div class="grid grid-cols-12">
+        <article class="teaser">
+          <x-heading type="h2" title="Forum Spezial" subtitle="Weitere Veranstaltungen" class="mb-10x lg:mb-12x" />
+        </article>
+      </div>
       @if ($annual_program->publishedArticlesSpecial)
         <div class="grid grid-cols-12">
           @foreach($annual_program->publishedArticlesSpecial as $article)
