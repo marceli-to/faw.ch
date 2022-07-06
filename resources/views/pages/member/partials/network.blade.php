a18fabbe424da4fd16a99dd0a617c2f45e0c38af
Marcel Stadelmann <m@marceli.to>
2022-07-06 08:42:44 +0200

    bf

--- resources/views/pages/member/partials/network.blade.php
+++ resources/views/pages/member/partials/network.blade.php
@@ -5,7 +5,7 @@
     @foreach($partners as $partner)
       <div class="mb-8x md:mb-10x">
         @if ($partner->url)
-          <a href="{{ $partner->url }}" target="_blank" title="Webseite {{ $partner->name }}">{{ $partner->name }}<br>{{ $partner->url }}</a>
+          <a href="{{ $partner->url }}" target="_blank" title="Webseite {{ $partner->name }}">{{ $partner->name }}</a>
         @else
           {{ $partner->name }}
         @endif
