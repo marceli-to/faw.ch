181343dc7db86379f2a8cd93e7b3aacf4a8695b5
Marcel Stadelmann <m@marceli.to>
2024-05-16 16:46:56 +0200

    wip

--- resources/views/components/card-sponsor.blade.php
+++ resources/views/components/card-sponsor.blade.php
@@ -1,4 +1,4 @@
-<div class="card-sponsor" >
+<div class="card-sponsor flex justify-center" >
   <a href="{{ $uri }}" target="_blank" title="{{ $title ?? '' }}">
     <img src="/assets/media/sponsoren/{{ $logo }}" height="200" width="400" {{ $attributes }}>
   </a>
